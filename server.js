e9e6557150e40dd3d91545ae0274a3e473d51924
taylynnharman <dyer.taylynn@gmail.com>
2024-11-02 12:36:53 -0600

    install cors

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const taskRoutes = require("./routes/tasks");
 //const myRoutes = require("./routes");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
+const cors = require("cors");
 
 app
   .use(express.json())
   .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(express.urlencoded({ extended: true }))
   //.use("/", myRoutes)
-  .use("/tasks", taskRoutes);
+  .use("/tasks", taskRoutes)
+  .use(cors());
 
 // Connect to MongoDB
 mongoDB.connectDb();
